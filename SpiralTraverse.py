8b143b88d75759202ab1a18b40f4122648479154
Talib-ilahi <270878360+Talib-90@users.noreply.github.com>
2026-04-08 17:15:16 +0500

    Modify Spiral Traverse code

--- SpiralTraverse.py
+++ SpiralTraverse.py
@@ -21,15 +21,11 @@
 #         endRow -= 1
 #         startingColumn += 1
 #         endCol -= 1
-    
+
 #     return result
 
-array = [
-    [1, 2, 3, 4],
-    [12, 13, 14, 5],
-    [11, 16, 15, 6],
-    [10, 9, 8, 7]
-]
+array = [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]
+
 
 # # O(N) T | O(N) S
 def spiralTraverse(array):
@@ -37,12 +33,13 @@ def spiralTraverse(array):
     spiralFill(array, 0, len(array) - 1, 0, len(array[0]) - 1, result)
     return result
 
+
 def spiralFill(array, startingRow, endRow, startingCol, endCol, result):
     if startingRow > endRow or startingCol > endCol:
         return
-    
+
     for col in range(startingCol, endCol + 1):
-            result.append(array[startingRow][col])
+        result.append(array[startingRow][col])
 
     for row in range(startingRow + 1, endRow + 1):
         result.append(array[row][endCol])
@@ -53,6 +50,9 @@ def spiralFill(array, startingRow, endRow, startingCol, endCol, result):
     for row in reversed(range(startingRow + 1, endRow)):
         result.append(array[row][startingCol])
 
-    return spiralFill(array, startingRow + 1, endRow - 1, startingCol + 1, endCol - 1, result)
+    return spiralFill(
+        array, startingRow + 1, endRow - 1, startingCol + 1, endCol - 1, result
+    )
+
 
 print(spiralTraverse(array))
